3698816c71497af3ee930b1b1a97a2d4c49aef85
Sixpounder <micheal.fomin@gmail.com>
2017-06-30 05:26:48 -0700

    LOOKS like end of tutorials. before rest API new project

--- music/views.py
+++ music/views.py
@@ -2,6 +2,12 @@ from django.views import generic
 from .models import Album
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login
+from django.views.generic import View
+from .forms import UserForm
+
+
 
 class IndexView(generic.ListView):
     template_name = 'music/index.html'
@@ -25,6 +31,40 @@ class AlbumUpdate(UpdateView):
 class AlbumDelete(DeleteView):
     model = Album
     success_url = reverse_lazy('music:index')
+
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'music/registraton_form.html'
+
+    #display blanck
+    def get(self,request):
+        form = self.form_class(None)
+        return render(request, self.template_name,{'form':form})
+
+    # submit form data
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+
+            user = form.save(commit=False)
+
+            #normalized data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+            # returns users object if credentials are correct
+            user = authenticate(password=password, username=username)
+
+            if user is not  None:
+
+                if user.is_active:
+                    login(request,user)
+                    return redirect('music:index')
+
+        return render(request, self.template_name, {'form': form})
 
 # from django.http import HttpResponse
 # from django.template import loader
